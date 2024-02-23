3697240d68bf4bc1a5f4982f847f13222ae5df0c
David Bosschaert <bosschae@adobe.com>
2024-02-23 10:45:57 +0000

    Add codecov

--- code-coverage.sh
+++ code-coverage.sh
@@ -1,7 +1,7 @@
 GRCOV_DIR=~/apps/grcov
 echo Directory containing the grcov executable: $GRCOV_DIR
 
-cargo clean 
+cargo clean
 
 echo Run tests producing raw coverage data...
 CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
@@ -9,7 +9,7 @@ CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-t
 echo Execute grcov to collect coverage data and produce HTML report...
 $GRCOV_DIR/grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 
-echo Deleting the *.profraw files from the current directory
+# echo Deleting the *.profraw files from the current directory
 # rm *.profraw
 
 # open target/coverage/html/index.html
