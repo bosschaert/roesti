ef649d81bd5b7e67c081871e85558aeea6536a2d
David Bosschaert <bosschae@adobe.com>
2024-02-23 10:51:44 +0000

    Small fix

--- code-coverage.sh
+++ code-coverage.sh
@@ -8,7 +8,7 @@ echo Run tests producing raw coverage data...
 CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
 
 echo Execute grcov to collect coverage data and produce HTML report...
-$GRCOV_DIR/grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 
 # echo Deleting the *.profraw files from the current directory
 # rm *.profraw
