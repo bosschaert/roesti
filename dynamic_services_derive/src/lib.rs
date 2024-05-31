1c98e5d20211261a9ffaffe4f92c109c16335ed5
David Bosschaert <bosschae@adobe.com>
2024-05-31 14:16:15 +0100

    Doing some parsing

--- dynamic_services_derive/src/lib.rs
+++ dynamic_services_derive/src/lib.rs
@@ -1,6 +1,9 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn;
+use proc_macro::{TokenStream};
+use quote::{quote, quote_spanned};
+use syn::{self, DataStruct, LitStr};
+use syn::{Data, DataEnum, DataUnion, DeriveInput, Error, Fields, Result, token};
+use proc_macro2::Span;
+use proc_macro2::TokenStream as TokenStream2;
 
 #[proc_macro_derive(DynamicServices, attributes(inject))]
 pub fn dynamic_services_derive(input: TokenStream) -> TokenStream {
@@ -9,13 +12,19 @@ pub fn dynamic_services_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
 
     // Build the trait implementation
-    impl_dynamic_services(&ast)
+    impl_dynamic_services(ast)
 }
 
-fn impl_dynamic_services(ast: &syn::DeriveInput) -> TokenStream {
+fn impl_dynamic_services(ast: syn::DeriveInput) -> TokenStream {
     println!("Given ast: {:?}", quote!(#ast));
+    println!("Name {} attrs {}", ast.ident, ast.attrs.len());
 
-    let name = &ast.ident;
+    let _ = TokenStream::from(match impl_my_trait(ast.clone()) {
+        | Ok(it) => it,
+        | Err(err) => err.to_compile_error(),
+    });
+
+    let name = ast.ident;
     let gen = quote! {
         impl #name<'_> {
             pub fn blah(&self) {
@@ -31,6 +40,57 @@ fn impl_dynamic_services(ast: &syn::DeriveInput) -> TokenStream {
     gen.into()
 }
 
+
+fn impl_my_trait (ast: DeriveInput)
+  -> Result<TokenStream2>
+{ Ok ({
+    let name = ast.ident;
+    let fields = match ast.data {
+        | Data::Enum(DataEnum { enum_token: token::Enum { span }, ..})
+        | Data::Union(DataUnion { union_token: token::Union { span }, ..})
+        => {
+            return Err(Error::new(span, "expected a struct"));
+        },
+        | Data::Struct(DataStruct { fields: Fields::Named(it), .. })
+        => {
+            it
+        },
+        | Data::Struct(_)
+        => {
+            return Err(Error::new(Span::call_site(), "expected a struct with named fields"));
+        },
+    };
+
+    for f in fields.named.iter() {
+        println!("Field: {:?}", f.ident);
+        f.attrs.iter().for_each(|a| {
+            println!("Attr: {:?} - {}", a.path().get_ident(),
+            a.path().get_ident().unwrap());
+        });
+    }
+
+    let data_expanded_members = fields.named.into_iter().map(|field| {
+        let field_name = field.ident.unwrap();
+        let span = field_name.span();
+        let field_name_stringified =
+            LitStr::new(&field_name.to_string(), span);
+        quote_spanned! { span =>
+            make_number(#field_name_stringified, &self.#field_name)
+        }
+
+    });
+    quote! {
+        impl RuleSystem for #name {
+            fn do_something(self: &'_ Self) -> Output {
+                Output {
+                    data: vec![
+                        #(#data_expanded_members ,)*
+                    ],
+                }
+            }
+        }
+    }
+})}
 /*
 #[proc_macro_derive(DynamicServices)]
 pub fn dynamic_services_derive(input: TokenStream) -> TokenStream {
